3ece85c127eae89b8c7d9d214d4a87cdf99ebba2
Yuliia Dvoinos <69351064+YuliiaDvoinos@users.noreply.github.com>
2020-12-09 00:24:05 +0200

    upd t-4

---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+const formatString = function (string) {
+    const maxLength = 40;
+    const formattingResult = string.length >= maxLength ? `${string.slice(0, 40)}...`  : string;
+    return formattingResult;
+
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// вернется форматированная строка
